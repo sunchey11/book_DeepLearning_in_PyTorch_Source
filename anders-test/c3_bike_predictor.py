8ad9275b2195a44fbf1632be47520f05a625c3d5
sunchey11 <sunchey11@users.noreply.github.com>
2023-06-28 17:10:31 +0800

    修改线性预测的代码

--- anders-test/c3_bike_predictor.py
+++ anders-test/c3_bike_predictor.py
@@ -5,10 +5,14 @@ import pandas as pd #读取csv文件的库
 import matplotlib.pyplot as plt
 import torch
 import torch.optim as optim
-
+import os
+# 此文件所在的路径
+print(os.path.abspath(__file__))
+print(os.path.split(os.path.abspath(__file__)))
+file_dir = os.path.split(os.path.abspath(__file__))[0]
+data_path = os.path.join(file_dir, "Bike-Sharing-Dataset/hour.csv")
 
 #读取数据到内存中，rides为一个dataframe对象
-data_path = 'Bike-Sharing-Dataset/hour.csv'
 rides = pd.read_csv(data_path)
 
 #看看数据长什么样子
--- anders-test/myc3/gen-data.py
+++ anders-test/myc3/gen-data.py
@@ -5,7 +5,7 @@ import torch  #导入torch包
 data_num=200
 x = torch.linspace(1, data_num, data_num).type(torch.FloatTensor) #linspace可以生成0-100之间的均匀的100个数字
 rand = torch.randn(data_num) * 10 #随机生成100个满足标准正态分布的随机数，均值为0，方差为1.将这个数字乘以10，标准方差变为10
-y = 0.1*x + rand #将x和rand相加，得到伪造的标签数据y。所以(x,y)应能近似地落在y=x这条直线上
+y = 2*x+100 + rand #将x和rand相加，得到伪造的标签数据y。所以(x,y)应能近似地落在y=x这条直线上
 
 d = {'x': x, 'y': y}
 df = pd.DataFrame(data=d)
